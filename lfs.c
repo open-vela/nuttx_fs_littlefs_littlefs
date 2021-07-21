3a952156803224cda3855eb641a4ace779e59306
Colin Foster <colin.foster@in-advantage.com>
2021-07-21 08:56:21 -0700

    Fail mount when the block size changes
    When the on-disk block size doesn't match the config block size, it is
    possible to get file corruption. For instance, if the num blocks was
    0x200 and we re-mount with 0x100 files could be corrupt.
    
    If we re-mount with a larger number of blocks things should be safer,
    but could be handled with a resize option or perhaps a mount flag to
    ignore this parameter.

--- lfs.c
+++ lfs.c
@@ -3774,6 +3774,12 @@ static int lfs_rawmount(lfs_t *lfs, const struct lfs_config *cfg) {
 
                 lfs->attr_max = superblock.attr_max;
             }
+
+            if (superblock.block_count != lfs->cfg->block_count)
+            {
+                err = LFS_ERR_CORRUPT;
+                goto cleanup;
+            }
         }
 
         // has gstate?
